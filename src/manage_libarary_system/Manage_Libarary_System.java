f1f731d33938ef581f7c331ca64688fc70ea3d7f
Trinh Hung Tuan <trinhhungtuan1@gmail.com>
2026-05-13 01:35:40 +0700

    update moi nhat

--- src/manage_libarary_system/Manage_Libarary_System.java
+++ src/manage_libarary_system/Manage_Libarary_System.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package manage_libarary_system;
 
-/**
- *
- * @author TRINH HUNG TUAN
- */
+
 public class Manage_Libarary_System {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        System.out.println("xuat ra man hinh di");
+}
 }
