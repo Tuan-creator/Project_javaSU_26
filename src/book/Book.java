7f082225b49d8b5b4a99e61a5c80fea8142d8055
C4uBeTuKy <phamhoanchau4@gmail.com>
2026-05-28 03:33:47 +0700

    Update Book.java

--- src/book/Book.java
+++ src/book/Book.java
@@ -17,17 +17,23 @@ public class Book {
     private int quantity;
  
     
-    public Book()
-    {
-        
-    }
 
     public Book(String bookId, String Title, String author, String genre, int pubYear, int quantity){
         if (bookId == null || bookId.trim().isEmpty()){
             throw new IllegalArgumentException("Book ID cannot be null or empty");
         }
         if (Title == null || Title.trim().isEmpty()){
-            throw new IllegalArgumentException("Book ID cannot be null or empty");
+            throw new IllegalArgumentException("Title cannot be null or empty");
+        }
+        if (author == null || author.trim().isEmpty()){
+            throw new IllegalArgumentException("author cannot be null or empty");
+        }
+          if (genre == null || genre.trim().isEmpty()){
+            throw new IllegalArgumentException("genre cannot be null or empty");
+        }
+          if (pubYear > 2026 || pubYear <= 0){
+            throw new IllegalArgumentException("Invalid publication year");
+        }
         this.bookId = bookId;
         this.Title = Title;
         this.author = author;
@@ -40,36 +46,51 @@ public class Book {
         return bookId;
     }
     public void setbookId(String bookId){
+        if (bookId == null || bookId.trim().isEmpty()){
+            throw new IllegalArgumentException("Book ID cannot be null or empty");
+    }
         this.bookId = bookId;   
     }
     public String getTitle(){
         return Title;
     }
     public void setTitle(String Title){
+        if (Title == null || Title.trim().isEmpty()){
+            throw new IllegalArgumentException("Title cannot be null or empty");
+    }
         this.Title = Title;
     }
     public String getauthor(){
         return author;
     }
     public void setauthor(String author){
+        if (author == null || author.trim().isEmpty()){
+            throw new IllegalArgumentException("author ID cannot be null or empty");
+    }
         this.author = author;
     }
     public String getgenre(){
         return genre;
     }
     public void setgenre(String genre){
+        if (genre == null || genre.trim().isEmpty()){
+            throw new IllegalArgumentException("genre cannot be null or empty");
+    }
         this.genre = genre;
     }
     public int getpubYear(){
         return pubYear;
     }
     public void setpubYear(int pubYear){
+        if (pubYear > 2026 || pubYear <= 0){
+            throw new IllegalArgumentException("Invalid publication year");
+    }
         this.pubYear = pubYear;
     }
     public int getquantity(){
         return quantity;
     }
-    public void setquantity(){
+    public void setquantity(int quantity){
         if(this.quantity>=0){
             this.quantity = quantity;
         }else{
