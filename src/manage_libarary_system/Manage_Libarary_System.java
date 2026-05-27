802053e3bd19c19143d2813eefc98fc51883f65c
Nhân nè <thnhan20612@gmail.com>
2026-05-28 01:09:09 +0700

    Update Manage_Libarary_System.java

--- src/manage_libarary_system/Manage_Libarary_System.java
+++ src/manage_libarary_system/Manage_Libarary_System.java
@@ -9,5 +9,49 @@ package manage_libarary_system;
  * @author TRINH HUNG TUAN
  */
 public class Manage_Libarary_System {
-    
+    private Book book;
+    private Member member;
+    private BorrowingTransaction transaction;
+
+    public void addBook(Book book) {
+        this.book = book;
+        System.out.println("Book added successfully.");
+    }
+
+    public void registerMember(Member member) {
+        this.member = member;
+        System.out.println("Member added successfully.");
+    }
+
+    public void borrowBook(BorrowingTransaction transaction) {
+        if (book != null) {
+            if (book.getQuantity() > 0) {
+                book.setQuantity(book.getQuantity() - 1);
+                this.transaction = transaction;
+                System.out.println("Borrow book successfully.");
+            } else {
+                System.out.println("Book out of stock.");
+            }
+        } else {
+            System.out.println("No book found.");
+        }
+    }
+
+    public void returnBook() {
+        if (book != null) {
+            book.setQuantity(book.getQuantity() + 1);
+            System.out.println("Return successfully.");
+        } else {
+            System.out.println("No book found.");
+        }
+    }
+
+    public void generateReports() {
+        System.out.println("===== REPORT =====");
+        if (transaction != null) {
+            transaction.displayTransaction();
+        } else {
+            System.out.println("No transaction.");
+        }
+    }
 }
