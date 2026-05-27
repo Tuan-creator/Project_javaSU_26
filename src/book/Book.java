ed5f4a4ca4b53e33d97e0ddfee29c9e7ce39d8b1
C4uBeTuKy <phamhoanchau4@gmail.com>
2026-05-27 23:38:42 +0700

    Update Book.java

--- src/book/Book.java
+++ src/book/Book.java
@@ -9,5 +9,63 @@ package book;
  * @author TRINH HUNG TUAN
  */
 public class Book {
-    
+    private String bookId;
+    private String Title;
+    private String author;
+    private String genre;
+    private int pubYear;
+    private int quantity;
+    private boolean isValiable;
+
+    public Book(String bookId, String Title, String author, String author, String genre, int pubYear, int quantity){
+        this.bookId = bookId;
+        this.Title = Title;
+        this.author = author;
+        this.genre = genre;
+        this.pubYear = pubYear;
+        this.quantity = quantity;
+    }
+    public String getbookId(){
+        return bookId;
+    }
+    public void setbookId(String bookId){
+        this.bookId = bookId;   
+    }
+    public String getTitle(){
+        return Title;
+    }
+    public void setTitle(String Title){
+        this.Title = Title;
+    }
+    public String getauthor(){
+        return author;
+    }
+    public void setauthor(String author){
+        this.author = author;
+    }
+    public String getgenre(){
+        return genre;
+    }
+    public void setgenre(String genre){
+        this.genre = genre;
+    }
+    public int getpubYear(){
+        return pubYear;
+    }
+    public void setpubYear(int pubYear){
+        this.pubYear = pubYear;
+    }
+    public int getquantity(){
+        return quantity;
+    }
+    public void setquantity(int quantity){
+        if(>=0){
+            this.quantity = quantity;
+        }else{
+            System.out.println("Quantity cannot be negative");
+        }
+    }
+    public boolean isValiable(int quantity){
+        return isValiable > 0;
+    }
 }
